9d4a0b7d883e5e5c060fa4a8175b442e11c33b13
cj <cj@csail.mit.edu>
2016-10-17 11:27:24 -0400

    update tsc

--- client/test/extension.test.ts
+++ client/test/extension.test.ts
@@ -4,19 +4,19 @@
 //
 
 // The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
+// import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-import * as myExtension from '../src/extension';
+// // You can import and use all API from the 'vscode' module
+// // as well as import your extension to test it
+// // import * as vscode from 'vscode';
+// import * as myExtension from '../src/extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+// // Defines a Mocha test suite to group tests of similar kind together
+// suite("Extension Tests", () => {
 
-	// Defines a Mocha unit test
-	test("Something 1", () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
-	});
-});
+// 	// Defines a Mocha unit test
+// 	test("Something 1", () => {
+// 		assert.equal(-1, [1, 2, 3].indexOf(5));
+// 		assert.equal(-1, [1, 2, 3].indexOf(0));
+// 	});
+// });
