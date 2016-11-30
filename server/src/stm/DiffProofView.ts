066c715dacd9b737a5e95be3dfe5c176d754da0a
cj <cj@csail.mit.edu>
2016-11-30 01:13:15 -0500

    fix #85

--- server/src/stm/DiffProofView.ts
+++ server/src/stm/DiffProofView.ts
@@ -7,42 +7,80 @@ import * as server from '../server';
 
 
 
-function diffHypotheses(oldHyps: Hypothesis[], newHyps: Hypothesis[]) {
+function diffHypotheses(oldHyps: Hypothesis[], newHyps: Hypothesis[]) : Hypothesis[] {
+  const results : Hypothesis[] = [];
   newHyps.forEach((hyp,idxHyp) => {
-    var oldHypIdx = idxHyp;
-    var oldHyp = oldHyps[oldHypIdx];
+    let oldHypIdx = idxHyp;
+    let oldHyp = oldHyps[oldHypIdx];
     if(oldHyp === undefined || oldHyp.identifier !== hyp.identifier) {
       oldHypIdx = oldHyps.findIndex((h) => h.identifier === hyp.identifier)
       oldHyp = oldHyps[oldHypIdx];
     }
 
     if(oldHyp === undefined)
-      hyp.diff = HypothesisDifference.New;
+      results.push({diff: HypothesisDifference.New, expression: hyp.expression, identifier: hyp.identifier, relation: hyp.relation})
     else {
       const diff = text.diffText(oldHyp.expression,hyp.expression);
-      hyp.expression = diff.text;
-      hyp.diff = diff.different ? HypothesisDifference.Changed : HypothesisDifference.None
+      results.push({
+        diff: diff.different ? HypothesisDifference.Changed : HypothesisDifference.None,
+        expression: diff.text,
+        identifier: hyp.identifier,
+        relation: hyp.relation})
     }
   })
+  return results;
 }
 
-function diffGoals(oldGoals: Goal[], newGoals: Goal[]) {
-  if(!newGoals || !oldGoals)
-    return;
-  newGoals.forEach((g,idxGoal) => {
-    if(oldGoals[idxGoal] !== undefined) {
-      diffHypotheses(oldGoals[idxGoal].hypotheses, g.hypotheses);
-      g.goal = text.diffText(oldGoals[idxGoal].goal,g.goal).text;
+function diffGoal(oldGoal: Goal, newGoal: Goal) : Goal {
+  if(oldGoal.id === newGoal.id)
+    return newGoal;
+  return {
+    hypotheses: diffHypotheses(oldGoal.hypotheses, newGoal.hypotheses),
+    goal: text.diffText(oldGoal.goal,newGoal.goal).text,
+    id: newGoal.id
+  }
+}
+
+function diffGoals(oldGoals: Goal[], newGoals: Goal[]) : Goal[] {
+  if(!newGoals || !oldGoals || oldGoals.length <= 0)
+    return newGoals;
+
+console.log(`[${oldGoals.map(g=>g.id).join(',')}] --> [${newGoals.map(g=>g.id).join(',')}]`);
+  const results : Goal[] = [];
+  const old = oldGoals.filter(o => newGoals.findIndex(n => n.id === o.id) === -1);
+  let oIdx = 0;
+  for(let nIdx = 0; nIdx < newGoals.length; ++nIdx) {
+    const hyp = newGoals[nIdx];
+    let o = oldGoals.findIndex(g => g.id === hyp.id);
+    if(o >= 0 || !old[oIdx]) {
+      results.push(hyp);
+    } else {
+      results.push(diffGoal(old[oIdx], hyp));
+      ++oIdx;
     }
-  })
+  }
+
+
+  // newGoals.forEach((g,idxGoal) => {
+  //   if(oldGoals[idxGoal] !== undefined)
+  //     results.push(diffGoal(oldGoals[idxGoal], g));
+  // })
+
+  return results;
 
 }
 
-export function diffProofView(oldState: ProofView, newState: ProofView) {
+export function diffProofView(oldState: ProofView, newState: ProofView) : ProofView {
   try {
-    diffGoals(oldState.goals, newState.goals);
+    return {
+      goals: diffGoals(oldState.goals, newState.goals),
+      abandonedGoals: newState.abandonedGoals,
+      backgroundGoals: newState.backgroundGoals,
+      shelvedGoals: newState.shelvedGoals,
+    }
   } catch(err) {
     server.connection.console.error('diffGoals threw an exception: ' + err.toString());
+    return newState;
   }
 }
 
