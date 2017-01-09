62b8732cadec666706e5510075b9b34e1e7bd520
siegebell <siegebell@gmail.com>
2017-01-08 22:05:21 -0500

    refactor coqtop

---
+++ server/src/coqtop/CoqTop.ts
@@ -0,0 +1,291 @@
+'use strict';
+
+import * as net from 'net'; 
+import * as util from 'util';
+import * as path from 'path';
+import * as events from 'events'; 
+import * as stream from 'stream'; 
+import * as coqXml from './xml-protocol/coq-xml';
+import * as vscode from 'vscode-languageserver';
+
+import * as coqProto from './coq-proto';
+import {ChildProcess, exec, spawn} from 'child_process';
+import {CoqTopSettings, LtacProfTactic, LtacProfResults} from '../protocol';
+import * as fs from 'fs';
+import * as os from 'os';
+import * as xmlTypes from './xml-protocol/CoqXmlProtocolTypes';
+import {AnnotatedText, normalizeText, textToDisplayString} from '../util/AnnotatedText';
+import {createDeserializer} from './xml-protocol/deserialize';
+
+/** Coqtop was interrupted; call cancelled */
+export class Interrupted {
+  constructor(
+    public stateId: number)
+  {}
+  public toString() {
+    return 'Coqtop Interrupted';
+  }
+}
+
+/** A fatal error of coqtop */
+export class CoqtopSpawnError {
+  constructor(private path: string, private message: string)
+  {}
+
+  public get binPath() : string {
+    return this.path;
+  }
+
+  public toString() {
+    return "Could not start coqtop: " + this.path + (this.message? "\n" + this.message : "");
+  }
+}
+/** A call did not succeed; a nonfatal error */
+export class CallFailure {
+  constructor(
+    public message: AnnotatedText,
+    public stateId?: number,
+    public range?: coqProto.Location)
+  {
+    this.message = normalizeText(this.message);
+  }
+  public toString() {
+    return textToDisplayString(this.message) +
+      (this.range || this.stateId
+        ? "  (" +
+          (this.range ? `offsets ${this.range.start}-${this.range.stop}` : (this.stateId ? " " : "")) +
+          (this.stateId ? ` of stateId ${this.stateId}` : "")
+          + ")"
+        : "")
+  }
+}
+
+export interface InitResult {
+  stateId: number;
+}
+export interface AddResult {
+  stateId: number;
+  unfocusedStateId?: number;
+  message: AnnotatedText;
+}
+export interface EditAtFocusResult {
+  stateId: number;
+  qedStateId: number;
+  oldStateIdTip: number;
+}
+export interface EditAtResult {
+  enterFocus?: EditAtFocusResult;
+}
+export interface ProofView {
+  goals: coqProto.Subgoal[];
+  backgroundGoals: coqProto.UnfocusedGoalStack
+  shelvedGoals: coqProto.Subgoal[];
+  abandonedGoals: coqProto.Subgoal[];
+}
+
+
+
+export type NoProofTag = {mode: 'no-proof'}
+export type ProofModeTag = {mode: 'proof'}
+export type NoProofResult = NoProofTag & {}
+export type ProofModeResult = ProofModeTag & ProofView
+export type GoalResult = NoProofResult | ProofModeResult
+
+
+
+  
+
+export interface EventCallbacks {
+  onFeedback? : (feedback: coqProto.StateFeedback) => void;
+  onMessage? : (msg: coqProto.Message) => void;
+  onClosed?: (error?: string) => void;
+}
+
+export function detectVersion(coqtopModule: string, cwd: string, console?: {log: (string)=>void, warn: (string)=>void}) : Promise<string|null> {
+  if(console)
+    console.log('exec: ' + coqtopModule + ' -v');
+  return new Promise<string>((resolve,reject) => {
+    try {
+      const coqtop = spawn(coqtopModule, ['-v'], {detached: false, cwd: cwd});
+      let result = "";
+
+      coqtop.stdout.on('data', (data:string) => {
+        result += data
+      });
+
+      coqtop.on('close', (code:number) => {
+        const ver = /^\s*The Coq Proof Assistant, version (.+?)\s/.exec(result);
+        // if(!ver)
+        //   console.warn('Could not detect coqtop version');
+        resolve(!ver ? undefined : ver[1]);
+      });
+      coqtop.on('error', (code:number) => {
+        // console.warn(`Could not start coqtop; error code: ${code}`)
+        reject(new CoqtopSpawnError(coqtopModule, `error code: ${code}`));
+      });
+    } catch(err) {
+      reject(new CoqtopSpawnError(coqtopModule, err));
+    }
+  })
+}
+
+export interface IdeSlave {
+  connect(version: string, mainR: stream.Readable, mainW: stream.Writable, controlR: stream.Readable, controlW: stream.Writable);
+  dispose() : void;
+  coqInterrupt() : Promise<boolean>;
+  coqInit() : Promise<InitResult>;
+  coqQuit() : Promise<void>;
+  coqGoal() : Promise<GoalResult>;
+  getStatus(force: boolean) : Promise<coqProto.CoqStatus>;
+  coqAddCommand(command: string, editId: number, stateId: number, verbose?: boolean) : Promise<AddResult>;
+  coqEditAt(stateId: number) : Promise<EditAtResult>;
+  coqLtacProfilingResults(stateId?: number, routeId?: number) : Promise<void>;
+  coqResizeWindow(columns: number) : Promise<void>;  
+  coqQuery(query: string, stateId?: number, routeId?: number) : Promise<AnnotatedText>;
+  coqGetOptions(options: CoqOptions) : Promise<void>;
+  coqSetOptions(options: CoqOptions) : Promise<void>;
+}
+
+
+export interface CoqTop {
+  dispose(): void;
+  isRunning() : boolean;  
+  getVersion() : string;
+  startCoq(settings?: CoqTopSettings) : Promise<InitResult>;
+  setupCoqTop(wrapper: string|null) : Promise<void>;
+
+  /** Start coqtop.
+   * Use two ports: one for reading & one for writing; i.e. HOST:READPORT:WRITEPORT
+   */
+  setupCoqTopReadAndWritePorts() : Promise<void>;
+  coqInterrupt() : Promise<boolean>;
+  coqInit() : Promise<InitResult>;
+  coqQuit() : Promise<void>;
+  coqGoal() : Promise<GoalResult>;
+  getStatus(force: boolean) : Promise<coqProto.CoqStatus>;
+  coqAddCommand(command: string, editId: number, stateId: number, verbose?: boolean) : Promise<AddResult>;
+  coqEditAt(stateId: number) : Promise<EditAtResult>;
+  coqLtacProfilingResults(stateId?: number, routeId?: number) : Promise<void>;
+  coqResizeWindow(columns: number) : Promise<void>;  
+  coqQuery(query: string, stateId?: number, routeId?: number) : Promise<AnnotatedText>;
+  coqGetOptions(options: CoqOptions) : Promise<void>;
+  coqSetOptions(options: CoqOptions) : Promise<void>;
+}
+
+export interface CoqOptions {
+  asymmetricPatterns?: boolean,
+  atomicLoad?: boolean,
+  automaticCoercionsImport?: boolean,
+  automaticIntroduction?: boolean,
+  booleanEqualitySchemes?: boolean,
+  bracketingLastIntroductionPattern?: boolean,
+  bulletBehavior?: string; // enum {Strict,
+  subproofsCaseAnalysisSchemes?: boolean,
+  compatNotations?: boolean,
+  congruenceDepth?: number,
+  congruenceVerbose?: boolean,
+  contextualImplicit?: boolean,
+  debugAuto?: boolean,
+  debugEauto?: boolean,
+  debugRAKAM?: boolean,
+  debugTacticUnification?: boolean,
+  debugTrivial?: boolean,
+  debugUnification?: boolean,
+  decidableEqualitySchemes?: boolean,
+  defaultClearingUsedHypotheses?: boolean,
+  defaultGoalSelector?: number,
+  defaultProofMode?: string; // enum {Classic,
+  defaultProofUsing?: any,
+  defaultTimeout?: number,
+  dependentPropositionsElimination?: boolean,
+  discriminateIntroduction?: boolean,
+  dumpBytecode?: boolean,
+  eliminationSchemes?: boolean,
+  equalityScheme?: boolean,
+  extractionAutoInline?: boolean,
+  extractionConservativeTypes?: boolean,
+  extractionFileComment?: string,
+  extractionFlag?: number,
+  extractionKeepSingleton?: boolean,
+  extractionOptimize?: boolean,
+  extractionSafeImplicits?: boolean,
+  extractionTypeExpand?: boolean,
+  firstorderDepth?: number,
+  hideObligations?: boolean,
+  implicitArguments?: boolean,
+  infoAuto?: boolean,
+  infoEauto?: boolean,
+  infoLevel?: any,
+  infoTrivial?: boolean,
+  injectionL2RPatternOrder?: boolean,
+  injectionOnProofs?: boolean,
+  inlineLevel?: number,
+  intuitionIffUnfolding?: boolean,
+  intuitionNegationUnfolding?: boolean,
+  kernelTermSharing?: boolean,
+  keyedUnification?: boolean,
+  looseHintBehavior?: string; // enum {Lax,
+  maximalImplicitInsertion?: boolean,
+  nonrecursiveEliminationSchemes?: boolean,
+  parsingExplicit?: boolean,
+  primitiveProjections?: boolean,
+  printingAll?: boolean,
+  printingCoercions?: boolean,
+  printingDepth?: number,
+  printingExistentialInstances?: boolean,
+  printingImplicit?: boolean,
+  printingImplicitDefensive?: boolean,
+  printingMatching?: boolean,
+  printingNotations?: boolean,
+  printingPrimitiveProjectionCompatibility?: boolean,
+  printingPrimitiveProjectionParameters?: boolean,
+  printingProjections?: boolean,
+  printingRecords?: boolean,
+  printingSynth?: boolean,
+  printingUniverses?: boolean,
+  printingWidth?: number,
+  printingWildcard?: boolean,
+  programMode?: boolean,
+  proofUsingClearUnused?: boolean,
+  recordEliminationSchemes?: boolean,
+  regularSubstTactic?: boolean,
+  reversiblePatternImplicit?: boolean,
+  rewritingSchemes?: boolean,
+  shortModulePrinting?: boolean,
+  shrinkObligations?: boolean,
+  simplIsCbn?: boolean,
+  standardPropositionEliminationNames?: boolean,
+  strictImplicit?: boolean,
+  strictProofs?: boolean,
+  strictUniverseDeclaration?: boolean,
+  stronglyStrictImplicit?: boolean,
+  suggestProofUsing?: boolean,
+  tacticCompatContext?: boolean,
+  tacticEvarsPatternUnification?: boolean,
+  transparentObligations?: boolean,
+  typeclassResolutionAfterApply?: boolean,
+  typeclassResolutionForConversion?: boolean,
+  typeclassesDebug?: boolean,
+  typeclassesDependencyOrder?: boolean,
+  typeclassesDepth?: any,
+  typeclassesModuloEta?: boolean,
+  typeclassesStrictResolution?: boolean,
+  typeclassesUniqueInstances?: boolean,
+  typeclassesUniqueSolutions?: boolean,
+  universalLemmaUnderConjunction?: boolean,
+  universeMinimizationToSet?: boolean,
+  universePolymorphism?: boolean,
+  verboseCompatNotations?: boolean,
+  // Asynchronous options:
+  function_debug?: boolean,
+  function_raw_tcc?: boolean,
+  functionalInductionRewriteDependent?: boolean,
+  hypsLimit?: any,
+  ltacDebug?: boolean,
+  silent?: boolean,
+  undo?: any,
+  // [DEPRECATED] Tables: Search Blacklist Printing Coercion Printing If Printing Let Printing Record Printing Constructor
+  // [DEPRECATED] Extraction AccessOpaque: boolean;
+  // [DEPRECATED] Refine Instance Mode: boolean;
+  // [DEPRECATED] Tactic Pattern Unification: boolean;
+}
---
+++ server/src/coqtop/CoqTop8.ts
@@ -0,0 +1,328 @@
+'use strict';
+
+import * as net from 'net'; 
+import * as util from 'util';
+import * as path from 'path';
+import * as events from 'events'; 
+// var xml2js = require('xml2js');
+// import * as stream from 'stream'; 
+import * as coqXml from './xml-protocol/coq-xml';
+import * as vscode from 'vscode-languageserver';
+
+import * as coqProto from './coq-proto';
+import {ChildProcess, exec, spawn} from 'child_process';
+import {CoqTopSettings, LtacProfTactic, LtacProfResults} from '../protocol';
+import * as fs from 'fs';
+import * as os from 'os';
+import * as xmlTypes from './xml-protocol/CoqXmlProtocolTypes';
+import {AnnotatedText, normalizeText, textToDisplayString} from '../util/AnnotatedText';
+import {createDeserializer} from './xml-protocol/deserialize';
+
+import * as coqtop from './coqtop';
+export {Interrupted, CoqtopSpawnError, CallFailure, EventCallbacks} from './coqtop';
+export {InitResult, AddResult, EditAtFocusResult, EditAtResult, ProofView} from './coqtop';
+export {NoProofTag, ProofModeTag, NoProofResult, ProofModeResult, GoalResult} from './coqtop';
+import {Interrupted, CoqtopSpawnError, CallFailure, EventCallbacks} from './coqtop';
+import {InitResult, AddResult, EditAtFocusResult, EditAtResult, ProofView} from './coqtop';
+import {NoProofTag, ProofModeTag, NoProofResult, ProofModeResult, GoalResult} from './coqtop';
+import {IdeSlave as IdeSlave8, IdeSlaveState} from './IdeSlave8';
+
+export class CoqTop extends IdeSlave8 implements coqtop.CoqTop {
+  private mainChannelServer: net.Server;
+  private mainChannelServer2: net.Server;
+  private controlChannelServer: net.Server;
+  private controlChannelServer2: net.Server;
+  // private mainChannelR : net.Socket;
+  // private mainChannelW : net.Socket;
+  // private controlChannelW : net.Socket;
+  // private controlChannelR : net.Socket;
+  // private console: vscode.RemoteConsole;
+  private coqtopProc : ChildProcess = null;
+  // private parser : coqXml.XmlStream;
+  // private callbacks: EventCallbacks;
+  private readyToListen: Thenable<void>[];
+  private settings : CoqTopSettings;
+  private scriptFile : string;
+  private projectRoot: string;
+  // private supportsInterruptCall = false;
+  private coqtopVersion : string;
+
+  constructor(settings : CoqTopSettings, scriptFile: string, projectRoot: string, console: vscode.RemoteConsole, callbacks?: EventCallbacks) {
+    super(console, callbacks);
+    this.settings = settings;
+    this.scriptFile = scriptFile;
+    this.projectRoot = projectRoot;
+    // this.console = console;
+    // this.callbacks = callbacks;
+    this.mainChannelServer = net.createServer();
+    this.mainChannelServer2 = net.createServer();
+    this.controlChannelServer = net.createServer();
+    this.controlChannelServer2 = net.createServer();
+    this.mainChannelServer.maxConnections = 1;
+    this.mainChannelServer2.maxConnections = 1;
+    this.controlChannelServer.maxConnections = 1;
+    this.controlChannelServer2.maxConnections = 1;
+    
+    this.readyToListen = [
+      this.startListening(this.mainChannelServer),
+      this.startListening(this.mainChannelServer2),
+      this.startListening(this.controlChannelServer),
+      this.startListening(this.controlChannelServer2)
+    ];
+
+
+    // this.resetCoq(coqPath);
+  }
+
+  public /* override */ dispose() {
+    super.dispose();
+    if(this.coqtopProc) {
+      try {
+        this.coqtopProc.kill();
+        if(this.coqtopProc.connected)
+          this.coqtopProc.disconnect();
+      } catch(e) {}
+      this.coqtopProc = null;
+    }
+    this.coqtopProc = undefined;
+  }
+
+  public isRunning() : boolean {
+    return this.coqtopProc != null;
+  }
+
+  public async startCoq(settings?: CoqTopSettings) : Promise<InitResult> {
+    if(this.state !== IdeSlaveState.Disconnected)
+      throw new CoqtopSpawnError(this.coqtopBin, "coqtop is already started");
+
+    if(settings)
+      this.settings = settings;    
+    this.console.log('start');
+
+    this.coqtopVersion = await coqtop.detectVersion(this.coqtopBin, this.projectRoot, this.console);
+    if(this.coqtopVersion)
+      this.console.log(`Detected coqtop version ${this.coqtopVersion}`)
+    else
+      this.console.warn(`Could not detect coqtop version`)
+
+    const wrapper = this.findWrapper();
+    if (wrapper !== null)
+      await this.setupCoqTop(wrapper);
+    else
+      await this.setupCoqTopReadAndWritePorts();
+    
+    return await this.coqInit();
+  }
+
+  
+  protected async /* override */ checkState() : Promise<void> {
+    if(this.coqtopProc === null)
+      this.startCoq();
+    super.checkState();
+  }
+  
+  private startListening(server: net.Server) : Promise<void> {
+    const port = 0;
+    const host = 'localhost';
+    return new Promise<void>((resolve,reject) => {
+      server.listen({port: port, host: host}, (err:any) => {
+        if (err)
+          reject(err);
+        else {
+          this.console.log(`Listening at ${server.address().address}:${server.address().port}`);
+          resolve();
+        }
+      });
+    });
+  }
+  
+  private acceptConnection(server: net.Server, name:string) : Promise<net.Socket> {
+    return new Promise<net.Socket>((resolve) => {
+      server.once('connection', (socket:net.Socket) => {
+        this.console.log(`Client connected on ${name} (port ${socket.localPort})`);
+        // socket.setEncoding('utf8');
+        // // if (dataHandler)
+        //   socket.on('data', (data:string) => dataHandler(data));
+        // socket.on('error', (err:any) => this.onCoqTopError(err.toString() + ` (${name})`));
+        resolve(socket);
+      });
+    });
+  }
+  
+  private findWrapper() : string|null {
+    const autoWrapper = path.join(__dirname, '../../../', 'coqtopw.exe');
+
+    if(this.settings.wrapper && this.settings.wrapper !== "" && fs.existsSync(this.settings.wrapper))
+      return this.settings.wrapper;
+    else if(this.settings.autoUseWrapper && os.platform() === 'win32' && fs.existsSync(autoWrapper))
+      return autoWrapper;
+    else
+      return null;
+  }
+
+  public getVersion() {
+    return this.coqtopVersion;
+  }
+
+  // public async resetCoq(settings?: CoqTopSettings) : Promise<InitResult> {
+  //   if(settings)
+  //     this.settings = settings;    
+  //   this.console.log('reset');
+  //   this.cleanup(undefined);
+
+  //   this.coqtopVersion = await coqtop.detectVersion(this.coqtopBin, this.projectRoot, this.console);
+  //   if(this.coqtopVersion)
+  //     this.console.log(`Detected coqtop version ${this.coqtopVersion}`)
+  //   else
+  //     this.console.warn(`Could not detect coqtop version`)
+
+  //   const wrapper = this.findWrapper();
+  //   if (wrapper !== null)
+  //     await this.setupCoqTop(wrapper);
+  //   else
+  //     await this.setupCoqTopReadAndWritePorts();
+    
+  //   return await this.coqInit();
+  // }
+
+  public async setupCoqTop(wrapper: string|null) : Promise<void> {
+    await Promise.all(this.readyToListen);
+
+    var mainAddr = this.mainChannelServer.address();
+    var controlAddr = this.controlChannelServer.address();
+    var mainAddressArg = mainAddr.address + ':' + mainAddr.port;
+    var controlAddressArg = controlAddr.address + ':' + controlAddr.port;
+
+    try {
+      const scriptUri = decodeURIComponent(this.scriptFile); 
+      if(wrapper !== null) {
+        const traceFile = (scriptUri.startsWith("file:///") && this.settings.traceXmlProtocol)
+          ? scriptUri.substring("file:///".length) + ".coq-trace.xml"
+          : undefined;
+        this.startCoqTop(this.spawnCoqTopWrapper(wrapper, mainAddressArg, controlAddressArg, traceFile));
+      } else
+        this.startCoqTop(this.spawnCoqTop(mainAddressArg, controlAddressArg));
+    } catch(error) {
+      this.console.error('Could not spawn coqtop: ' + error);
+      throw new CoqtopSpawnError(this.coqtopBin, error);
+    }
+
+    let channels = await Promise.all([
+        this.acceptConnection(this.mainChannelServer, 'main channel'), //, 'main channel R', (data) => this.onMainChannelR(data)),
+        this.acceptConnection(this.controlChannelServer, 'control channel'),
+      ]);
+
+    this.connect(this.coqtopVersion, channels[0], channels[0], channels[1], channels[1])
+  }
+  
+  /** Start coqtop.
+   * Use two ports: one for reading & one for writing; i.e. HOST:READPORT:WRITEPORT
+   */
+  public async setupCoqTopReadAndWritePorts() : Promise<void> {    
+    await Promise.all(this.readyToListen);
+
+    var mainAddr = this.mainChannelServer.address();
+    var mainPortW = this.mainChannelServer2.address().port;
+    var controlAddr = this.controlChannelServer.address();
+    var controlPortW = this.controlChannelServer2.address().port;
+    var mainAddressArg = mainAddr.address + ':' + mainAddr.port + ':' + mainPortW;
+    var controlAddressArg = controlAddr.address + ':' + controlAddr.port + ':' + controlPortW;
+
+    try {
+      this.startCoqTop(this.spawnCoqTop(mainAddressArg, controlAddressArg));
+    } catch(error) {
+      this.console.error('Could not spawn coqtop: ' + error);
+      throw new CoqtopSpawnError(this.coqtopBin, error);
+    }
+
+    let channels = await Promise.all([
+        this.acceptConnection(this.mainChannelServer, 'main channel R'), //, 'main channel R', (data) => this.onMainChannelR(data)),
+        this.acceptConnection(this.mainChannelServer2, 'main channel W'),
+        this.acceptConnection(this.controlChannelServer, 'control channel R'),
+        this.acceptConnection(this.controlChannelServer2, 'control channel W'),
+      ]);
+
+    this.connect(this.coqtopVersion, channels[0], channels[1], channels[2], channels[3])
+  }
+
+  private startCoqTop(process : ChildProcess) {
+    this.coqtopProc = process;
+    this.console.log(`coqtop started with pid ${this.coqtopProc.pid}`);
+    this.coqtopProc.stdout.on('data', (data:string) => this.coqtopOut(data))
+    this.coqtopProc.on('exit', (code:number) => {
+      this.console.log('coqtop exited with code: ' + code);
+      this.dispose();
+    });
+    this.coqtopProc.stderr.on('data', (data:string) => {
+      this.console.log('coqtop-stderr: ' + data);
+    });
+    this.coqtopProc.on('close', (code:number) => {
+      this.console.log('coqtop closed with code: ' + code);
+      this.dispose();
+      if(this.isRunning())
+        this.callbacks.onClosed('coqtop closed with code: ' + code);
+    });
+    this.coqtopProc.on('error', (code:number) => {
+      this.console.log('coqtop could not be started: ' + code);
+      this.dispose();
+      if(this.isRunning())
+        this.callbacks.onClosed('coqtop closed with code: ' + code);
+    });
+    // this.coqtopProc.stdin.write('\n');
+  }
+
+  private coqtopOut(data:string) {
+    this.console.log('coqtop-stdout:' + data);
+  }
+
+  private get coqtopBin() {
+    return path.join(this.settings.binPath.trim(), 'coqtop');
+  }
+
+  private spawnCoqTop(mainAddr : string, controlAddr: string) {
+    var coqtopModule = this.coqtopBin;
+    // var coqtopModule = 'cmd';
+    var args = [
+      // '/D /C', this.coqPath + '/coqtop.exe',
+      '-main-channel', mainAddr,
+      '-control-channel', controlAddr,
+      '-ideslave',
+      '-async-proofs', 'on'
+      ].concat(this.settings.args);
+    this.console.log('exec: ' + coqtopModule + ' ' + args.join(' '));
+    return spawn(coqtopModule, args, {detached: false, cwd: this.projectRoot});
+  }
+
+  private spawnCoqTopWrapper(wrapper: string, mainAddr : string, controlAddr: string, traceFile?: string) : ChildProcess {
+    this.useInterruptMessage = true;
+    var coqtopModule = wrapper;
+    var args = [
+      // '/D /C', this.coqPath + '/coqtop.exe',
+      '-coqtopbin', this.coqtopBin,
+      '-main-channel', mainAddr,
+      '-control-channel', controlAddr,
+      '-ideslave',
+      '-async-proofs', 'on'
+      ]
+      .concat(traceFile ? ['-tracefile', traceFile] : [])
+      .concat(this.settings.args);
+    this.console.log('exec: ' + coqtopModule + ' ' + args.join(' '));
+    return spawn(coqtopModule, args, {detached: false, cwd: this.projectRoot});
+  }
+
+  public /* override */ async coqInterrupt() : Promise<boolean> {
+    if(!this.coqtopProc)
+      return false;
+    else if(!super.coqInterrupt()) {
+      this.console.log('--------------------------------');
+      this.console.log('Sending SIGINT');
+      this.coqtopProc.kill("SIGINT");
+      return true;
+    }
+  }
+}
+
+
+
+
+
