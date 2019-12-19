fae03449ef9b57e8a61b684d503fb9222b373f77
Ramkumar Ramachandra <artagnon@gmail.com>
2019-12-19 08:50:04 +0100

    [webpack.config.js] Wire into flattened build
    Use a toplevel webpack.config.js, accessible from the toplevel package.json.
    The compile command is now "webpack && tsc -b". Use a webpack plugin to copy
    over html and css files, cleaning things up a bit.
    
    While at it, flatten the layout of the built files. The .d.ts files are
    still nested within directories, but we don't use them anyway.

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const webpack = require("webpack");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  name: "webview",
+  mode: "development",
+  entry: {
+    goals: path.resolve(__dirname, "html_views/src/goals/goals.ts"),
+    ltacprof: path.resolve(__dirname, "html_views/src/ltacprof/ltacprof.ts")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "out/html_views")
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      jQuery: "jquery",
+      $: "jquery",
+      jquery: "jquery"
+    }),
+    new CopyPlugin([
+      {
+        from: "html_views/src/**/*.html",
+        to: path.resolve(__dirname, "out", "html_views"),
+        flatten: true
+      },
+      {
+        from: "html_views/src/**/*.css",
+        to: path.resolve(__dirname, "out", "html_views"),
+        flatten: true
+      }
+    ])
+  ]
+};
