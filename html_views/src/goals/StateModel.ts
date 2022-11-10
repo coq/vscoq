9682464f0de93538c3eac09032935ae8f589a248
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2022-11-10 12:11:39 +0000

    Format code

--- html_views/src/goals/StateModel.ts
+++ html_views/src/goals/StateModel.ts
@@ -1,225 +1,252 @@
-import * as $ from 'jquery';
-import { ProofView, UnfocusedGoalStack, HypothesisDifference, TextDifference, AnnotatedText, ScopedText, Hypothesis, Goal, CommandResult } from './protocol';
-import { makeBreakingText } from './ui-util';
+import * as $ from "jquery";
+import {
+  ProofView,
+  UnfocusedGoalStack,
+  HypothesisDifference,
+  TextDifference,
+  AnnotatedText,
+  ScopedText,
+  Hypothesis,
+  Goal,
+  CommandResult,
+} from "./protocol";
+import { makeBreakingText } from "./ui-util";
 
-function countUnfocusedGoals(u: UnfocusedGoalStack|undefined) : number {
-  if(!u)
-    return 0;
+function countUnfocusedGoals(u: UnfocusedGoalStack | undefined): number {
+  if (!u) return 0;
   return u.before.length + u.after.length + countUnfocusedGoals(u.next);
 }
 
 function countAllGoals(state: ProofView): number {
   const result =
-    (state.goals ? state.goals.length : 0)
-    + countUnfocusedGoals(state.backgroundGoals)
-    + (state.abandonedGoals ? state.abandonedGoals.length : 0)
-    + (state.shelvedGoals ? state.shelvedGoals.length : 0);
+    (state.goals ? state.goals.length : 0) +
+    countUnfocusedGoals(state.backgroundGoals) +
+    (state.abandonedGoals ? state.abandonedGoals.length : 0) +
+    (state.shelvedGoals ? state.shelvedGoals.length : 0);
   return result;
 }
 
 function getDifferenceClass(diff: HypothesisDifference) {
-  switch(diff) {
+  switch (diff) {
     case HypothesisDifference.Changed:
-      return ' changed';
+      return " changed";
     case HypothesisDifference.New:
-      return ' new';
+      return " new";
     case HypothesisDifference.MovedUp:
-      return ' movedUp';
+      return " movedUp";
     case HypothesisDifference.MovedDown:
-      return ' movedDown';
+      return " movedDown";
     default:
-      return '';
+      return "";
   }
 }
 
-function getTextDiffClass(diff?: TextDifference) : string {
-  if(diff === "added")
-    return 'charsAdded'
-  if(diff === "removed")
-    return 'charsRemoved'
-  else
-    return ''
+function getTextDiffClass(diff?: TextDifference): string {
+  if (diff === "added") return "charsAdded";
+  if (diff === "removed") return "charsRemoved";
+  else return "";
 }
 
 function isScopedText(text: AnnotatedText): text is ScopedText {
-  return text.hasOwnProperty('scope');
+  return text.hasOwnProperty("scope");
 }
-
 
 let hasSubstitutions = false;
 
-function createAnnotatedText(text: AnnotatedText) : HTMLElement[] {
-  function helper(text: AnnotatedText) : (Text | HTMLElement)[] {
-    if(typeof text === 'string')
-      return makeBreakingText(text)
-    else if(text instanceof Array)
-      return Array.prototype.concat(...text.map(helper))
-    else if(isScopedText(text))
-      return text.scope.trim()!== ""
-        ? [$('<span>')
-          .addClass(text.scope.replace('.', '-'))
-          .append(helper(text.text))
-          .get(0)]
+function createAnnotatedText(text: AnnotatedText): HTMLElement[] {
+  function helper(text: AnnotatedText): (Text | HTMLElement)[] {
+    if (typeof text === "string") return makeBreakingText(text);
+    else if (text instanceof Array)
+      return Array.prototype.concat(...text.map(helper));
+    else if (isScopedText(text))
+      return text.scope.trim() !== ""
+        ? [
+            $("<span>")
+              .addClass(text.scope.replace(".", "-"))
+              .append(helper(text.text))
+              .get(0),
+          ]
         : helper(text.text);
-    else if(text.substitution) {// TextAnnotation
+    else if (text.substitution) {
+      // TextAnnotation
       hasSubstitutions = true;
-      return [$('<span>')
-        .addClass('substitution')
-        .addClass(getTextDiffClass(text.diff))
-        .attr("subst",text.substitution)
-        .attr("title",text.text)
-        .append(makeBreakingText(text.text))
-        .get(0)];
-    } else // TextAnnotation
-      return [$('<span>')
-        .addClass(getTextDiffClass(text.diff))
-        .append(makeBreakingText(text.text))
-        .get(0)];
+      return [
+        $("<span>")
+          .addClass("substitution")
+          .addClass(getTextDiffClass(text.diff))
+          .attr("subst", text.substitution)
+          .attr("title", text.text)
+          .append(makeBreakingText(text.text))
+          .get(0),
+      ];
+    } // TextAnnotation
+    else
+      return [
+        $("<span>")
+          .addClass(getTextDiffClass(text.diff))
+          .append(makeBreakingText(text.text))
+          .get(0),
+      ];
   }
-  return [$('<span>')
-    .addClass('richpp')
-    .append(helper(text))
-    .get(0)];
+  return [$("<span>").addClass("richpp").append(helper(text)).get(0)];
 }
 
 function onDoubleClickBreakableText(event: JQueryMouseEventObject) {
   // var target = <Element>event.currentTarget;
-  if($(event.currentTarget).hasClass('hypothesis')) {
-    $(event.currentTarget).toggleClass('breakText');
-    $(event.currentTarget).toggleClass('noBreakText');
+  if ($(event.currentTarget).hasClass("hypothesis")) {
+    $(event.currentTarget).toggleClass("breakText");
+    $(event.currentTarget).toggleClass("noBreakText");
   }
 }
 
-
-function createHypothesis(hyp: Hypothesis) : JQuery {
-  return $('<li>')
-    .addClass('hypothesis')
-    .addClass('breakText')
+function createHypothesis(hyp: Hypothesis): JQuery {
+  return $("<li>")
+    .addClass("hypothesis")
+    .addClass("breakText")
     .addClass(getDifferenceClass(hyp.diff))
-    .append(
-      [ $('<span>').addClass('ident').text(hyp.identifier)
-      , $('<span>').addClass('rel').text(hyp.relation)
-      , $('<span>').addClass('expr')
-        .append($(createAnnotatedText(hyp.expression)))
-      ])
-    .on('dblclick', onDoubleClickBreakableText)
-
+    .append([
+      $("<span>").addClass("ident").text(hyp.identifier),
+      $("<span>").addClass("rel").text(hyp.relation),
+      $("<span>")
+        .addClass("expr")
+        .append($(createAnnotatedText(hyp.expression))),
+    ])
+    .on("dblclick", onDoubleClickBreakableText);
 }
 
 function createHypotheses(hyps: Hypothesis[]) {
-  return $('<ul>')
-    .addClass('hypotheses')
+  return $("<ul>")
+    .addClass("hypotheses")
     .append(hyps.map((hyp) => createHypothesis(hyp)));
 }
 
-function createGoal(goal: Goal, idx:number, count:number) {
-  let expr = $('<span>').addClass('expr');
+function createGoal(goal: Goal, idx: number, count: number) {
+  let expr = $("<span>").addClass("expr");
   expr.append($(createAnnotatedText(goal.goal)));
-  if (idx == 0) {expr.attr("id", "firstGoal")};
-  return $('<li>')
-    .addClass('goal')
-    .append(
-      [ $('<span>').addClass('goalId').text(`${idx+1}/${count}`)
-      , $('<span>').addClass('error')
-      , expr
-      ]);
-}
-
-function createFocusedGoals(goals: Goal[]) : JQuery {
-  return $('<ul>')
-    .addClass('goalsList')
-    .append(goals.map((g,i) => createGoal(g,i,goals.length)));
-}
-
-const givenUpAndShelvedGoalCountElement = document.getElementById("given-up-and-shelved-goal-count");
-export class StateModel {
-
-  constructor() {
+  if (idx == 0) {
+    expr.attr("id", "firstGoal");
   }
+  return $("<li>")
+    .addClass("goal")
+    .append([
+      $("<span>")
+        .addClass("goalId")
+        .text(`${idx + 1}/${count}`),
+      $("<span>").addClass("error"),
+      expr,
+    ]);
+}
 
+function createFocusedGoals(goals: Goal[]): JQuery {
+  return $("<ul>")
+    .addClass("goalsList")
+    .append(goals.map((g, i) => createGoal(g, i, goals.length)));
+}
+
+const givenUpAndShelvedGoalCountElement = document.getElementById(
+  "given-up-and-shelved-goal-count"
+);
+export class StateModel {
+  constructor() {}
 
   private setMessage(message: string) {
-    $('#messages').text(message);
+    $("#messages").text(message);
   }
 
   private setErrorMessage(message: AnnotatedText) {
-    if(typeof message === 'string')
-      $('#error').text(message);
+    if (typeof message === "string") $("#error").text(message);
     else {
-      $('#error').append($(createAnnotatedText(message)));
+      $("#error").append($(createAnnotatedText(message)));
     }
   }
   private clearErrorMessage() {
-    $('#error').empty();
+    $("#error").empty();
   }
 
   public updateState(state: CommandResult) {
     try {
       hasSubstitutions = false;
       this.clearErrorMessage();
-      $('#stdout').text('');
+      $("#stdout").text("");
 
-      $('#states').empty();
+      $("#states").empty();
 
-      if(state.type === 'failure')
-        this.setErrorMessage(state.message);
-      else if(state.type === 'not-running')
-        this.setMessage('Not running.');
-      else if(state.type === 'no-proof')
-        this.setMessage('Not in proof mode.');
-      else if(state.type === 'interrupted')
-        this.setMessage("Interrupted.");
-      else if(state.type === 'proof-view') {
+      if (state.type === "failure") this.setErrorMessage(state.message);
+      else if (state.type === "not-running") this.setMessage("Not running.");
+      else if (state.type === "no-proof") this.setMessage("Not in proof mode.");
+      else if (state.type === "interrupted") this.setMessage("Interrupted.");
+      else if (state.type === "proof-view") {
         if (countAllGoals(state) == 0) {
           this.setMessage("No more subgoals.");
         } else if (state.goals) {
-          if (state.shelvedGoals.length > 0 || state.abandonedGoals.length > 0) {
+          if (
+            state.shelvedGoals.length > 0 ||
+            state.abandonedGoals.length > 0
+          ) {
             const shelvedGoalCountElement = document.createElement("div");
-            shelvedGoalCountElement.style.cssText = "font-weight: bold; color: cyan; padding: 2pt 10pt;";
+            shelvedGoalCountElement.style.cssText =
+              "font-weight: bold; color: cyan; padding: 2pt 10pt;";
             const givenUpGoalCountElement = document.createElement("div");
-            givenUpGoalCountElement.style.cssText = "font-weight: bold; color: orange; padding: 2pt 10pt;";
-            shelvedGoalCountElement.textContent = "Shelved goal count: " + state.shelvedGoals.length;
-            givenUpGoalCountElement.textContent = "Given up goal count: " + state.abandonedGoals.length;
+            givenUpGoalCountElement.style.cssText =
+              "font-weight: bold; color: orange; padding: 2pt 10pt;";
+            shelvedGoalCountElement.textContent =
+              "Shelved goal count: " + state.shelvedGoals.length;
+            givenUpGoalCountElement.textContent =
+              "Given up goal count: " + state.abandonedGoals.length;
 
             givenUpAndShelvedGoalCountElement.innerHTML = "";
             if (state.shelvedGoals.length > 0) {
-              givenUpAndShelvedGoalCountElement.appendChild(shelvedGoalCountElement);
-              $(givenUpAndShelvedGoalCountElement).append(createFocusedGoals(state.shelvedGoals));
+              givenUpAndShelvedGoalCountElement.appendChild(
+                shelvedGoalCountElement
+              );
+              $(givenUpAndShelvedGoalCountElement).append(
+                createFocusedGoals(state.shelvedGoals)
+              );
             }
             if (state.abandonedGoals.length > 0) {
-              givenUpAndShelvedGoalCountElement.appendChild(givenUpGoalCountElement);
-              $(givenUpAndShelvedGoalCountElement).append(createFocusedGoals(state.abandonedGoals));
+              givenUpAndShelvedGoalCountElement.appendChild(
+                givenUpGoalCountElement
+              );
+              $(givenUpAndShelvedGoalCountElement).append(
+                createFocusedGoals(state.abandonedGoals)
+              );
             }
           } else {
             givenUpAndShelvedGoalCountElement.innerHTML = "";
           }
-          if(state.goals.length > 0) {
+          if (state.goals.length > 0) {
             this.setMessage("");
-            $('#states')
-            .append(
-              [ createHypotheses(state.goals[0].hypotheses)
-              , createFocusedGoals(state.goals)
-            ])
+            $("#states").append([
+              createHypotheses(state.goals[0].hypotheses),
+              createFocusedGoals(state.goals),
+            ]);
             var elmnt = document.getElementById("firstGoal");
-            elmnt.scrollIntoView({block: "center", inline: "nearest"}); 
-          } else if (state.backgroundGoals && (state.backgroundGoals.before.length > 0 || state.backgroundGoals.after && state.backgroundGoals.after.length > 0 || state.backgroundGoals.next)) {
+            elmnt.scrollIntoView({ block: "center", inline: "nearest" });
+          } else if (
+            state.backgroundGoals &&
+            (state.backgroundGoals.before.length > 0 ||
+              (state.backgroundGoals.after &&
+                state.backgroundGoals.after.length > 0) ||
+              state.backgroundGoals.next)
+          ) {
             this.setMessage("There are unfocused goals.");
-          } else if (state.shelvedGoals.length > 0 ) {
+          } else if (state.shelvedGoals.length > 0) {
             this.setMessage("There are shelved goals. Try using `Unshelve`.");
           } else if (state.abandonedGoals.length > 0) {
-            this.setMessage("There are some goals you gave up. You need to go back and solve them, or use `Admitted.`.")
+            this.setMessage(
+              "There are some goals you gave up. You need to go back and solve them, or use `Admitted.`."
+            );
           } else
-          this.setMessage("No more subgoals. Internal invariant violated. Please report.")
+            this.setMessage(
+              "No more subgoals. Internal invariant violated. Please report."
+            );
         }
-        
-      if(hasSubstitutions)
-        $('#togglePrettifySymbols').removeClass("hidden")
-      else
-        $('#togglePrettifySymbols').addClass("hidden")
-    }
-    } catch(err) {
+
+        if (hasSubstitutions) $("#togglePrettifySymbols").removeClass("hidden");
+        else $("#togglePrettifySymbols").addClass("hidden");
+      }
+    } catch (err) {
       this.setMessage(err);
     }
   }
-
 }
