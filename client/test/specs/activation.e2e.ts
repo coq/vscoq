644b28d6efd126127a12a7c732a12f7f9c3332c5
Maxime Denes <maxime.denes@inria.fr>
2023-02-13 10:56:33 +0100

    wip

--- client/test/specs/activation.e2e.ts
+++ client/test/specs/activation.e2e.ts
@@ -1,18 +1,34 @@
+before('should load extension and open problems view', async () => {
+    this.timeout(180000);
+    const workbench = await browser.getWorkbench();
+    const bottomBar = await workbench.getBottomBar();
+    const problemsView = await bottomBar.openProblemsView();
+    return await browser.waitUntil(async () => {
+        const workbench = await browser.getWorkbench();
+        const notifications = await workbench.getNotifications();
+        for (const n of notifications) {
+          await n.dismiss();
+        }
+        const openNotifications = await workbench.getNotifications();
+        return openNotifications.length === 0;
+      });
+  });
+
 describe('VsCoq 2', () => {
     it('should be able to load VS Code', async () => {
-        const workbench = await browser.getWorkbench()
+        const workbench = await browser.getWorkbench();
         expect(await workbench.getTitleBar().getTitle())
-            .toBe('[Extension Development Host] basic.v - Visual Studio Code')
-    })
+            .toBe('[Extension Development Host] basic.v - Visual Studio Code');
+    });
 
     it('should get error feedback from language server', async () => {
-        const workbench = await browser.getWorkbench()
-        const bottomBar = await workbench.getBottomBar().wait()
-        const problemsView = await bottomBar.openProblemsView()
+        const workbench = await browser.getWorkbench();
+        const bottomBar = workbench.getBottomBar();
+        const problemsView = await bottomBar.openProblemsView();
         await browser.waitUntil(async () => {
-            const countBadge = await problemsView.getCountBadge()
-            return (await countBadge.isExisting())
-        }, { timeout: 30000 })
-        expect(await (await problemsView.getCountBadge()).getText()).toBe('1')
-    })
+            const countBadge = await problemsView.getCountBadge();
+            return (await countBadge.isExisting());
+        }, { timeout: 30000 });
+        expect(await (await problemsView.getCountBadge()).getText()).toBe('1');
+    });
 })
