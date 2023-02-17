3831c02fbbd4f4f473cfa9e8aceafe3eb5d4b376
Maxime Dénès <maxime.denes@inria.fr>
2023-02-17 17:14:41 +0100

    Add e2e test case for opening of multiple documents

--- client/src/test/suite/extension.test.ts
+++ client/src/test/suite/extension.test.ts
@@ -1,9 +1,16 @@
 import { expect } from 'expect';
+import * as path from 'node:path';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
+
+async function openTextFile(file : string) {
+  const docUri = vscode.Uri.file(path.resolve(__dirname, '../../../testFixture', file));
+  const doc = await vscode.workspace.openTextDocument(docUri);
+  await vscode.window.showTextDocument(doc);
+}
 
 suite('Should get diagnostics', function () {
 
@@ -11,8 +18,11 @@ suite('Should get diagnostics', function () {
 
 	test('Diagnoses an undefined ref error', async () => {
 
+		await openTextFile('basic.v');
+
 		const ext = vscode.extensions.getExtension('coq-community.vscoq')!;
 		await ext.activate();
+
 		await sleep(10000); // Wait for server initialization
 
 		const allDiagnostics = vscode.languages.getDiagnostics();
@@ -28,6 +38,25 @@ suite('Should get diagnostics', function () {
 		expect(diagnostic.message).toMatch(/The reference zar was not found.*/);
 		
 		expect(diagnostic.severity).toBe(vscode.DiagnosticSeverity.Error);
+	
+	});
+
+	test('Opens two files and gets feedback', async () => {
+
+		await openTextFile('basic.v');
+
+		const ext = vscode.extensions.getExtension('coq-community.vscoq')!;
+		await ext.activate();
+
+		await sleep(10000); // Wait for server initialization
+
+		await openTextFile('warn.v');
+
+		await sleep(10000); // Wait for server initialization
+
+		const allDiagnostics = vscode.languages.getDiagnostics();
+
+		expect(allDiagnostics.length).toBe(2);
 	
 	});
 
