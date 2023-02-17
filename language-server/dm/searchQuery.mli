a11bf6d1a3e6c50a0c8198d67abf18b72590eded
Maxime Dénès <maxime.denes@inria.fr>
2023-02-17 21:54:39 +0100

    Fix interfaces header
    I have no idea how we ended with the wrong headers. Note that our
    implementation files were ok.

--- language-server/dm/searchQuery.mli
+++ language-server/dm/searchQuery.mli
@@ -1,3 +1,16 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 VSCoq                                  *)
+(*                                                                        *)
+(*                   Copyright INRIA and contributors                     *)
+(*       (see version control and README file for authors & dates)        *)
+(*                                                                        *)
+(**************************************************************************)
+(*                                                                        *)
+(*   This file is distributed under the terms of the MIT License.         *)
+(*   See LICENSE file.                                                    *)
+(*                                                                        *)
+(**************************************************************************)
 open Lsp.LspData
 
 val query_feedback : notification Sel.event
