5d5510a3abd8ef5d5ce83611f6ed4d8f1c20381b
jakobis <jakobisraelsen@live.dk>
2023-03-17 10:27:58 +0100

    Remove getHyps and makeHyp

--- language-server/dm/completionSuggester.ml
+++ language-server/dm/completionSuggester.ml
@@ -10,18 +10,6 @@ end
 
 module Atomics = Set.Make(TypeCompare)
 
-let mk_hyp sigma d (env,l) =
-  let d' = CompactedDecl.to_named_context d in
-  let env' = List.fold_right Environ.push_named d' env in
-  let ids, typ = match d with
-  | CompactedDecl.LocalAssum (ids, typ) -> ids, typ
-  | CompactedDecl.LocalDef (ids,c,typ) -> ids, typ
-  in
-  let ids' = List.map (fun id -> Names.Id.to_string id.Context.binder_name) ids in
-  let typ' = pr_ltype_env env sigma typ in
-  let hyps = ids' |> List.map (fun id -> (id, Pp.string_of_ppcmds typ', "")) in
-  (env', hyps @ l)
-
 let get_goal_type st loc =
   let goal, sigma = 
     match DocumentManager.get_proof st loc with
@@ -259,19 +247,6 @@ let rank_choices algorithm =
   | SimpleTypeIntersection -> SimpleAtomics.rank
   | SplitTypeIntersection -> Split.rank
   | StructuredTypeEvaluation -> Structured.rank
-
-let get_hyps st loc =
-  let mk_hyps sigma goal =
-    let EvarInfo evi = Evd.find sigma goal in
-    let env = Evd.evar_filtered_env (Global.env ()) evi in
-    let min_env = Environ.reset_context env in
-    let (_env, hyps) =
-      Context.Compacted.fold (mk_hyp sigma)
-        (Termops.compact_named_context (Environ.named_context env)) ~init:(min_env,[]) in
-    hyps in
-
-  DocumentManager.get_proof st (Some loc)
-    |> Option.map (fun Proof.{ goals; sigma; _ } -> Option.cata (mk_hyps sigma) [] (List.nth_opt goals 0)) 
  
 let take n l =
   let rec sub_list n accu l =
