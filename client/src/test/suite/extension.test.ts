d860b4f831bda40cbb7ae493e317f77738cb5b57
Enrico Tassi <Enrico.Tassi@Inria.fr>
2023-05-24 16:12:16 +0200

    test for proof skip, bogus

--- client/src/test/suite/extension.test.ts
+++ client/src/test/suite/extension.test.ts
@@ -86,11 +86,9 @@ suite('Should get diagnostics', function () {
 		const ext = vscode.extensions.getExtension('coq-community.vscoq')!;
 		await ext.activate();
 
-		// vscode.workspace.getConfiguration().update('vscoq.proof.delegation','Skip');
 		vscode.workspace.getConfiguration().update('vscoq.proof.delegation','Delegate');
 
 		const doc1 = await openTextFile('delegate_proof.v');
-
 		const doc2 = await openTextFile('warn.v');
 
 		await sleep(10000); // Wait for server initialization
@@ -110,6 +108,35 @@ suite('Should get diagnostics', function () {
 	
 	});
 
+	// BOGUS TEST, since we get the diagnostics from the previous run!!!
+	test('Opens two files and gets feedback in the appropriate tab (with proof skip)', async () => {
+
+		const ext = vscode.extensions.getExtension('coq-community.vscoq')!;
+		await ext.activate();
+
+		vscode.workspace.getConfiguration().update('vscoq.proof.delegation','Skip');
+
+		const doc1 = await openTextFile('delegate_proof.v');
+
+		const doc2 = await openTextFile('warn.v');
+
+		await sleep(10000); // Wait for server initialization
+
+		const diagnostics1 = vscode.languages.getDiagnostics(doc1);
+		const diagnostics2 = vscode.languages.getDiagnostics(doc2);
+
+		// XXX these should not be there!!
+		expect(diagnostics1.length).toBe(2);
+		expect(diagnostics1[1].message).toMatch(/.*foobar was not found.*/);
+		expect(diagnostics1[1].severity).toBe(vscode.DiagnosticSeverity.Error);
+		expect(diagnostics1[0].message).toMatch(/.*Attempt to save an incomplete proof.*/);
+		expect(diagnostics1[0].severity).toBe(vscode.DiagnosticSeverity.Error);
+
+		expect(diagnostics2.length).toBe(1);
+		expect(diagnostics2[0].message).toMatch(/.*There is no flag or option.*/);
+		expect(diagnostics2[0].severity).toBe(vscode.DiagnosticSeverity.Error); // BUG, should be warning
+	
+	});
 
 });
 
