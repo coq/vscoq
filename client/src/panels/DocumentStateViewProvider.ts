70fc4120f30d2a94f6814e4b61bf3eedaa8cd3be
Romain Tetley <rtetley@inria.fr>
2023-05-25 14:55:05 +0200

    Adding a log channel to the document state provider.

--- client/src/panels/DocumentStateViewProvider.ts
+++ client/src/panels/DocumentStateViewProvider.ts
@@ -14,16 +14,20 @@ export class DocumentStateViewProvider implements TextDocumentContentProvider {
     readonly scheme = "vscoq-document-state";
     readonly uri = Uri.parse(this.scheme + "://doc");
 
+    private static _channel: any = window.createOutputChannel('vscoq-document-state-provider');
+
     constructor(
         private _client: Client
     ){ }
 
     async provideTextDocumentContent(uri: Uri, token: CancellationToken): Promise<string> {
+        DocumentStateViewProvider._channel.appendLine("Getting document state for uri: " + uri.toString());
         const textDocument = TextDocumentIdentifier.create(uri.toString());
         const params: DocumentStateRequest = {textDocument}; 
         const req = new RequestType<DocumentStateRequest, DocumentStateResponse, void>("vscoq/documentState");
         return this._client.sendRequest(req, params).then(
             (result: DocumentStateResponse) => {
+                DocumentStateViewProvider._channel.appendLine("Recieved response: " + result.document);
                 return result.document;
             }
         );   
