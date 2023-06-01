b3dc8c46210eba47c6faf23bd40c6a55a2ec6af6
Romain Tetley <rtetley@inria.fr>
2023-06-01 14:18:40 +0200

    Added a proof view test.

--- language-server/tests/dm_tests.ml
+++ language-server/tests/dm_tests.ml
@@ -119,7 +119,26 @@ let%test_unit "step_forward.delete_observe_id" =
   let st = DocumentManager.apply_text_edits st [Document.range_of_id doc s2.id,""] in
   [%test_pred: sentence_id option] (Option.equal Stateid.equal (Some s1.id)) (DocumentManager.Internal.observe_id st)
 
-
+  let%test_unit "step_forward.proof_view" =
+  let st, init_events = init "Definition x := 3. Lemma foo : x = 3." in 
+  let st, (s1, (s2, ())) = dm_parse st (P(P O)) in
+  let todo = Sel.(enqueue empty init_events) in
+  let st, events = DocumentManager.interpret_to_next st in
+  let todo = Sel.(enqueue todo events) in
+  let st = handle_events todo st in
+  let st, events = DocumentManager.interpret_to_next st in
+  let todo = Sel.(enqueue todo events) in
+  let st = handle_events todo st in
+  let st, events = DocumentManager.interpret_to_next st in
+  let todo = Sel.(enqueue todo events) in
+  let st = handle_events todo st in
+  [%test_pred: sentence_id option] (Option.equal Stateid.equal (Some s2.id)) (DocumentManager.Internal.observe_id st);
+  let data = DocumentManager.get_proof st None in
+  [%test_eq: bool] (Option.is_some data) true
+  (* 
+  let doc = DocumentManager.Internal.document st in
+  let st = DocumentManager.apply_text_edits st [Document.range_of_id doc s2.id,""] in 
+  [%test_pred: sentence_id option] (Option.equal Stateid.equal (Some s1.id)) (DocumentManager.Internal.observe_id st) *)
 
 (*
 let%test_unit "exec.insert" =
