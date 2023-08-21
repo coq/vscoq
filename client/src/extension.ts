fc98914f3e91b2f121018e714082b3f776aa8fd8
Romain Tetley <romain.tetley@inria.fr>
2023-08-21 15:41:27 +0200

    Correcting the toolchain manager:
    - We now use the server configuration provided by the toolchain manager
    - By default, we will always use the vscoqtop path provided in user settings
    - Otherwise we default to vscoqtop found in the PATH variable
    - We search for coq std lib with vscoqtop -where
    - All relevant eror messages and actions are sent to user

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -40,160 +40,179 @@ import {
 import { DocumentStateViewProvider } from './panels/DocumentStateViewProvider';
 import VsCoqToolchainManager from './utilities/toolchain';
 
-
 let client: Client;
 
 export function activate(context: ExtensionContext) {
+    
+    const coqTM = new VsCoqToolchainManager();
+    coqTM.intialize().then(
+        res => {
+            const serverOptions = coqTM.getServerConfiguration(); 
+            intializeExtension(serverOptions);
+        }, 
+        err => {
+            if(err.status === 1) {
+
+                window.showErrorMessage("vscoq2 requires an installation of vscoqtop. ", {title: "Get vscoqtop", id: 0})
+                    .then(act => {
+                        if(act?.id === 0) {
+                            commands.executeCommand("vscode.open", Uri.parse('https://github.com/coq-community/vscoq#installing-the-language-server'));
+                        }
+                    });
+
+            } else if (err.status === 2) {
+                window.showErrorMessage(err.errorMessage, {title: "Get Coq", id: 0})
+                    .then(act => {
+                        if(act?.id === 0) {
+                            commands.executeCommand("vscode.open", Uri.parse('https://coq.inria.fr/download'));
+                        }
+                        
+                    });
+            } else {
+                window.showErrorMessage("vscoq2 could not start for an unknown reason");
+            }
+
+        }
+    );
 
     function registerVscoqTextCommand(command: string, callback: (textEditor: TextEditor, ...args: any[]) => void) {
         context.subscriptions.push(commands.registerTextEditorCommand('extension.coq.' + command, callback));
     };
     
-	const config = workspace.getConfiguration('vscoq');
+    function intializeExtension(serverOptions: ServerOptions) {
+        const config = workspace.getConfiguration('vscoq');
 
-	let serverOptions: ServerOptions = {
-		/*
-		command: "perf",
-		args: ["record","--call-graph=dwarf", "--", config.path].concat(config.args)
-		*/
-		command: config.path,
-		args: config.args
-	};
+        let clientOptions: LanguageClientOptions = {
+            documentSelector: [{ scheme: 'file', language: 'coq' }],
+            initializationOptions: config
+        };
 
-	let clientOptions: LanguageClientOptions = {
-		documentSelector: [{ scheme: 'file', language: 'coq' }],
-        initializationOptions: config
-	};
-
-	// Create the language client and start the client.
-	client = new Client(
-		serverOptions,
-		clientOptions
-	);
-
-    //register the search view provider 
-    const searchProvider = new SearchViewProvider(context.extensionUri, client);
-    context.subscriptions.push(window.registerWebviewViewProvider(SearchViewProvider.viewType, searchProvider));
-
-    const documentStateProvider = new DocumentStateViewProvider(client); 
-    context.subscriptions.push(workspace.registerTextDocumentContentProvider("vscoq-document-state", documentStateProvider));
-
-    const launchQuery = (editor: TextEditor, type: string)=> {
-        const selection = editor.selection;
-        const {end, start} = selection; 
-        if(end.line !== start.line) {return;} //don't allow for multiline selections
-        //either use the user selection or if no selection than infer the word under the cursor
-        const wordAtCurorRange = (end.character !== start.character) ? selection : editor.document.getWordRangeAtPosition(end);
-        if(!wordAtCurorRange) {return; } //there is no word: do nothing
-        const queryText = editor.document.getText(wordAtCurorRange);
-        //focus on the query panel
-        commands.executeCommand('vscoq.search.focus');
-        //launch the query
-        searchProvider.launchQuery(queryText, type);
-    };
-
-    registerVscoqTextCommand('query.search', (editor) => launchQuery(editor, "search"));
-    registerVscoqTextCommand('query.about', (editor) => launchQuery(editor, "about"));
-    registerVscoqTextCommand('query.check', (editor) => launchQuery(editor, "check"));
-    registerVscoqTextCommand('query.locate', (editor) => launchQuery(editor, "locate"));
-    registerVscoqTextCommand('query.print', (editor) => launchQuery(editor, "print"));
-    registerVscoqTextCommand('addQueryTab', () => searchProvider.addTab());
-    registerVscoqTextCommand('collapseAllQueries', () => searchProvider.collapseAll());
-    registerVscoqTextCommand('expandAllQueries', () => searchProvider.expandAll());
-    registerVscoqTextCommand('interpretToPoint', (editor) => sendInterpretToPoint(editor, client));
-    registerVscoqTextCommand('interpretToEnd', (editor) => sendInterpretToEnd(editor, client));
-    registerVscoqTextCommand('stepForward', (editor) => sendStepForward(editor, client));
-    registerVscoqTextCommand('stepBackward', (editor) => sendStepBackward(editor, client));
-    registerVscoqTextCommand('documentState', async (editor) => {
-            
-        documentStateProvider.setDocumentUri(editor.document.uri);
-
-        const document = await workspace.openTextDocument(documentStateProvider.uri);
-
-        documentStateProvider.fire();
-
-        await window.showTextDocument(document, {
-            viewColumn: ViewColumn.Two,
-            preserveFocus: true,
-        }); 
-        
-    });
-
-    const coqTM = new VsCoqToolchainManager();
-
-	client.onReady()
-	.then(() => {
-        
-        checkVersion(client, context);
-
-		initializeDecorations(context);
-        
-        // I think vscode should handle this automatically, TODO: try again after implemeting client capabilities
-        context.subscriptions.push(workspace.onDidChangeConfiguration(event => updateServerOnConfigurationChange(event, context, client)));
-		
-        client.onNotification("vscoq/updateHighlights", (notification) => {
-        
-            client.saveHighlights(
-                notification.uri, 
-                notification.parsedRange, 
-                notification.processingRange, 
-                notification.processedRange
-            );
-        
-            client.updateHightlights();
-		});
-
-        client.onNotification("vscoq/moveCursor", (notification: MoveCursorNotification) => {
-            const {uri, range} = notification;
-            const editors = window.visibleTextEditors.filter(editor => {
-                return editor.document.uri.toString() === uri.toString();
-            });
-            if(workspace.getConfiguration('vscoq.proof.cursor').sticky === true &&
-            workspace.getConfiguration('vscoq.proof').mode === 0) {
-                editors.map(editor => {
-                    editor.selections = [new Selection(range.end, range.end)];
-                    editor.revealRange(range, TextEditorRevealType.Default);
-                });
-            }
-        });
-
-        client.onNotification("vscoq/searchResult", (searchResult: SearchCoqResult) => {
-            searchProvider.renderSearchResult(searchResult);
-        });
-
-        client.onNotification("vscoq/proofView", (proofView: ProofViewNotification) => {
-            const editor = window.activeTextEditor ? window.activeTextEditor : window.visibleTextEditors[0];
-            GoalPanel.proofViewNotification(context.extensionUri, editor, client, proofView);
-        });
-
-        client.onNotification("vscoq/coqFeedback", (notification: CoqFeedbackNotification) => {
-            notification.feedback.map((f: CoqFeedback) => {
-                client.writeToFeedbackChannel(f.channel, f.message);
-            });
-        });
-
-        let goalsHook = window.onDidChangeTextEditorSelection(
-            (evt: TextEditorSelectionChangeEvent) => {                    
-                if (evt.textEditor.document.languageId === "coq" 
-                    && workspace.getConfiguration('vscoq.proof').mode === 1
-                    && isMouseOrKeyboardEvent(evt)) 
-                {
-                    sendInterpretToPoint(evt.textEditor, client);
-                }
-            }
+        // Create the language client and start the client.
+        client = new Client(
+            serverOptions,
+            clientOptions
         );
 
-        window.onDidChangeActiveTextEditor(editor => {
-            client.updateHightlights();
+        //register the search view provider 
+        const searchProvider = new SearchViewProvider(context.extensionUri, client);
+        context.subscriptions.push(window.registerWebviewViewProvider(SearchViewProvider.viewType, searchProvider));
+
+        const documentStateProvider = new DocumentStateViewProvider(client); 
+        context.subscriptions.push(workspace.registerTextDocumentContentProvider("vscoq-document-state", documentStateProvider));
+
+        const launchQuery = (editor: TextEditor, type: string)=> {
+            const selection = editor.selection;
+            const {end, start} = selection; 
+            if(end.line !== start.line) {return;} //don't allow for multiline selections
+            //either use the user selection or if no selection than infer the word under the cursor
+            const wordAtCurorRange = (end.character !== start.character) ? selection : editor.document.getWordRangeAtPosition(end);
+            if(!wordAtCurorRange) {return; } //there is no word: do nothing
+            const queryText = editor.document.getText(wordAtCurorRange);
+            //focus on the query panel
+            commands.executeCommand('vscoq.search.focus');
+            //launch the query
+            searchProvider.launchQuery(queryText, type);
+        };
+
+        registerVscoqTextCommand('query.search', (editor) => launchQuery(editor, "search"));
+        registerVscoqTextCommand('query.about', (editor) => launchQuery(editor, "about"));
+        registerVscoqTextCommand('query.check', (editor) => launchQuery(editor, "check"));
+        registerVscoqTextCommand('query.locate', (editor) => launchQuery(editor, "locate"));
+        registerVscoqTextCommand('query.print', (editor) => launchQuery(editor, "print"));
+        registerVscoqTextCommand('addQueryTab', () => searchProvider.addTab());
+        registerVscoqTextCommand('collapseAllQueries', () => searchProvider.collapseAll());
+        registerVscoqTextCommand('expandAllQueries', () => searchProvider.expandAll());
+        registerVscoqTextCommand('interpretToPoint', (editor) => sendInterpretToPoint(editor, client));
+        registerVscoqTextCommand('interpretToEnd', (editor) => sendInterpretToEnd(editor, client));
+        registerVscoqTextCommand('stepForward', (editor) => sendStepForward(editor, client));
+        registerVscoqTextCommand('stepBackward', (editor) => sendStepBackward(editor, client));
+        registerVscoqTextCommand('documentState', async (editor) => {
+                
+            documentStateProvider.setDocumentUri(editor.document.uri);
+
+            const document = await workspace.openTextDocument(documentStateProvider.uri);
+
+            documentStateProvider.fire();
+
+            await window.showTextDocument(document, {
+                viewColumn: ViewColumn.Two,
+                preserveFocus: true,
+            }); 
+            
         });
 
-	});
-    
-    coqTM.performChecks();
+        client.onReady()
+        .then(() => {
+            
+            checkVersion(client, context);
 
-    // Start the client. This will also launch the server
-    client.start();
-    context.subscriptions.push(client);
+            initializeDecorations(context);
+            
+            // I think vscode should handle this automatically, TODO: try again after implemeting client capabilities
+            context.subscriptions.push(workspace.onDidChangeConfiguration(event => updateServerOnConfigurationChange(event, context, client)));
+            
+            client.onNotification("vscoq/updateHighlights", (notification) => {
+            
+                client.saveHighlights(
+                    notification.uri, 
+                    notification.parsedRange, 
+                    notification.processingRange, 
+                    notification.processedRange
+                );
+            
+                client.updateHightlights();
+            });
+
+            client.onNotification("vscoq/moveCursor", (notification: MoveCursorNotification) => {
+                const {uri, range} = notification;
+                const editors = window.visibleTextEditors.filter(editor => {
+                    return editor.document.uri.toString() === uri.toString();
+                });
+                if(workspace.getConfiguration('vscoq.proof.cursor').sticky === true &&
+                workspace.getConfiguration('vscoq.proof').mode === 0) {
+                    editors.map(editor => {
+                        editor.selections = [new Selection(range.end, range.end)];
+                        editor.revealRange(range, TextEditorRevealType.Default);
+                    });
+                }
+            });
+
+            client.onNotification("vscoq/searchResult", (searchResult: SearchCoqResult) => {
+                searchProvider.renderSearchResult(searchResult);
+            });
+
+            client.onNotification("vscoq/proofView", (proofView: ProofViewNotification) => {
+                const editor = window.activeTextEditor ? window.activeTextEditor : window.visibleTextEditors[0];
+                GoalPanel.proofViewNotification(context.extensionUri, editor, client, proofView);
+            });
+
+            client.onNotification("vscoq/coqFeedback", (notification: CoqFeedbackNotification) => {
+                notification.feedback.map((f: CoqFeedback) => {
+                    client.writeToFeedbackChannel(f.channel, f.message);
+                });
+            });
+
+            let goalsHook = window.onDidChangeTextEditorSelection(
+                (evt: TextEditorSelectionChangeEvent) => {                    
+                    if (evt.textEditor.document.languageId === "coq" 
+                        && workspace.getConfiguration('vscoq.proof').mode === 1
+                        && isMouseOrKeyboardEvent(evt)) 
+                    {
+                        sendInterpretToPoint(evt.textEditor, client);
+                    }
+                }
+            );
+
+            window.onDidChangeActiveTextEditor(editor => {
+                client.updateHightlights();
+            });
+
+        });
+
+        // Start the client. This will also launch the server
+        client.start();
+        context.subscriptions.push(client);
+    }	
 
 }
 
--- client/src/utilities/toolchain.ts
+++ client/src/utilities/toolchain.ts
@@ -1,29 +1,65 @@
 import { window, workspace } from 'vscode';
 import { Disposable } from "vscode-languageclient";
-import { exec } from 'child_process';
+import { ExecException, exec } from 'child_process';
 import * as path from 'path';
 import { isFileInFolder } from './fileHelper';
+import { ServerSessionOptions } from 'http2';
+import { ServerOptions } from 'vscode-languageclient/node';
+
+interface ToolchainResponse {
+    status: number; 
+    errorMessage?: string; 
+}
 
 export default class VsCoqToolchainManager implements Disposable {
     
     private static _channel: any = window.createOutputChannel('vscoq-toolchain-manager');
+    private _vscoqtopPath: string = ""; 
 
     public dispose(): void {
         
     }
-    
-    public performChecks() : void {
 
-        this.vscoqtopPath().then(vscoqtopPath => {
-            if(vscoqtopPath) {
-                VsCoqToolchainManager._channel.appendLine("Found vscoqtop, searching for coq libs");
-                this.checkIfCoqFound(vscoqtopPath);
-            } else {
-                VsCoqToolchainManager._channel.appendLine("Could not find vscoqtop, notiftying user");
-                window.showErrorMessage("Could not find vscoqtop, please provide a path in user settings");
-            }
+
+
+    public intialize() : Promise<ToolchainResponse> {
+        VsCoqToolchainManager._channel.appendLine("SEARCHING");
+        return new Promise((resolve, reject) => {
+            this.vscoqtopPath().then(vscoqtopPath => {
+                if(vscoqtopPath) {
+                    VsCoqToolchainManager._channel.appendLine("Found path: " + vscoqtopPath);
+                    this._vscoqtopPath = vscoqtopPath;
+                    this.findCoq().then(
+                        res => {
+                            resolve({
+                                status: 0
+                            });
+                        }, 
+                        err => {
+                            reject(err);
+                        }
+                    );
+
+                } else {
+                    VsCoqToolchainManager._channel.appendLine("Did not find path");
+                    reject({
+                        status: 1, 
+                        errorMessage: "Could not find vscoqtop"
+                    });
+                }
+            })
         });
+        
+    };
 
+    public getServerConfiguration() : ServerOptions {
+
+        const config = workspace.getConfiguration('vscoq');
+        const serverOptions : ServerOptions = {
+            command: this._vscoqtopPath, 
+            args: config.args
+        };
+        return serverOptions;
     };
 
     private getEnvPath() : string {
@@ -53,21 +89,19 @@ export default class VsCoqToolchainManager implements Disposable {
     private async vscoqtopPath () : Promise<string> {
         const vscoqtopPath = workspace.getConfiguration('vscoq').get('path') as string;
         if(vscoqtopPath) {
-            VsCoqToolchainManager._channel.appendLine("Found vscoqtop in user configuration: " + vscoqtopPath);
-            return path.dirname(vscoqtopPath); 
+            return vscoqtopPath; 
         }
         else {
-            VsCoqToolchainManager._channel.appendLine("No path set for vscoqtop in user configuration, querying PATH");
             return await this.searchForVscoqtopInPath();
         }
-
     }
 
     private async searchForVscoqtopInPath () : Promise<string> {
         const pathVars = this.splitEnvPath(this.getEnvPath());
         for(let i in pathVars) {
+            VsCoqToolchainManager._channel.appendLine(pathVars[i]);
             if(await isFileInFolder('vscoqtop', pathVars[i])) {
-                return pathVars[i];
+                return pathVars[i] + '/vscoqtop';
             }
         }
         return "";
@@ -77,21 +111,28 @@ export default class VsCoqToolchainManager implements Disposable {
     //or wherever the user specified by passing -coqlib. 
     //(since 8.18 -where does not only print where the prelude is expected to be, 
     //but also checks it exists).
-    private checkIfCoqFound(vscoqtopPath: string) {
-        const options = workspace.getConfiguration('vscoq').get('args') as string[];
+    private findCoq() : Promise<ToolchainResponse> {
+        
+        const config = workspace.getConfiguration('vscoq').get('args') as string[];
+        const options = ["-where"].concat(config);
+        const cmd = [this._vscoqtopPath].concat(options).join(' ');
 
-        const cmd = ["./vscoqtop -where"].concat(options).join(' ');
-        VsCoqToolchainManager._channel.appendLine("Launching command: " + cmd + " in folder: " + vscoqtopPath);
-        exec(cmd, {cwd: vscoqtopPath}, (error, stdout, stderr) => {
-            if(error) {
-                const errorMessage = `${vscoqtopPath} cannot run properly since the installation of coq standard library seems broken: ${stderr}\n
-                                        If this is not the coq installation you wish to use , set the right path in the settings panel.`;
-                VsCoqToolchainManager._channel.appendLine(errorMessage);
-                window.showErrorMessage(errorMessage);
-            } else {
-                VsCoqToolchainManager._channel.appendLine("Standard output: " + stdout);
-                VsCoqToolchainManager._channel.appendLine("Error output: " + stderr);
-            }
+        return new Promise((resolve, reject) => {
+            exec(cmd, (error, stdout, stderr) => {
+
+                if(error) {
+                    reject({
+                        status: 2, 
+                        errorMessage: `${this._vscoqtopPath} cannot run properly since the installation of coq standard library seems broken: ${stderr}\n
+                        If this is not the coq installation you wish to use , set the right path in the settings panel.`
+                    });
+                } else {
+                    resolve({
+                        status: 0
+                    });
+                }
+                
+            });
         });
     };
 
