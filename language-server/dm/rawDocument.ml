d144117ffcee1ca81d747bfbd08dfd86ea46836f
Romain Tetley <romain.tetley@inria.fr>
2023-10-12 16:34:00 +0200

    Remove unecessary funtion
    We will correct the position_of_loc in a another PR

--- language-server/dm/rawDocument.ml
+++ language-server/dm/rawDocument.ml
@@ -24,29 +24,20 @@ let compute_lines text_doc =
   let _,lines_locs = CList.fold_left_map (fun acc s -> let n = String.length s in n + acc + 1, acc) 0 lines in
   Array.of_list lines_locs
 
-let compute_lines_ocaml_lsp text_doc = 
-  let text = Lsp.Text_document.text text_doc in
-  let lines = String.split_on_char '\n' text in
-  let get_line_loc line_number = 
-    let pos = Position.{line = line_number; character = 0} in
-    Lsp.Text_document.absolute_position text_doc pos 
-  in
-  let lines_locs = List.init (List.length lines) get_line_loc in
-  Array.of_list lines_locs
-
 let compare_lines doc log = 
   let text_doc = doc.text in
   let lines = compute_lines text_doc in
-  let lines_ocaml = compute_lines_ocaml_lsp text_doc in
+  let lines_ocaml = compute_lines text_doc in
   let i = ref 0 in
   while(!i < Array.length lines) do log @@ Format.sprintf "%i <--> %i\n" lines.(!i) lines_ocaml.(!i); incr(i) done
 
 let create text =
   let text = Lsp.Text_document.make ~position_encoding:`UTF16 text in
-  { text; lines = compute_lines_ocaml_lsp text }
+  { text; lines = compute_lines text }
 
 let text t = Lsp.Text_document.text t.text
 
+(* TODO: DO THIS CORRECTLY IE USING UTF16 ENCODING *)
 let position_of_loc raw loc =
   let i = ref 0 in
   while (!i < Array.length raw.lines && raw.lines.(!i) <= loc) do incr(i) done;
@@ -66,17 +57,8 @@ let range_of_loc raw loc =
 
 let apply_text_edits raw edits = 
   let text_doc = Lsp.Text_document.apply_text_document_edits raw.text edits in
-  let new_lines = compute_lines_ocaml_lsp text_doc in
+  let new_lines = compute_lines text_doc in
   { text = text_doc; lines = new_lines}
-
-(* let apply_text_edit raw (Range.{start; end_}, editText) =
-  let start = loc_of_position raw start in
-  let stop = loc_of_position raw end_ in
-  let before = String.sub raw.text 0 start in
-  let after = String.sub raw.text stop (String.length raw.text - stop) in
-  let new_text = before ^ editText ^ after in (* FIXME avoid concatenation *)
-  let new_lines = compute_lines new_text in (* FIXME compute this incrementally *)
-  { text = new_text; lines = new_lines }, start  *)
 
 let word_at_position raw pos : string option =
   let r = Str.regexp {|\([a-zA-Z_][a-zA-Z_0-9]*\)|} in
