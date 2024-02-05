9b99cb63232d74b15d2ef877415a3bbed3d357c9
Romain Tetley <romain.tetley@inria.fr>
2024-02-05 16:48:20 +0100

    Nix build for different coq versions.
    Make devShells for coq 8.18, coq 8.19 and coq master.
    Run them all in the CI.

--- flake.nix
+++ flake.nix
@@ -5,16 +5,88 @@
 
     flake-utils.url = "github:numtide/flake-utils";
 
+    coq-master = { url = "github:coq/coq/fbaea89860348ca2b2ca485e52df7215bea27746"; }; # Should be kept in sync with PIN_COQ in CI workflow
+    coq-master.inputs.nixpkgs.follows = "nixpkgs";
+
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, coq-master }:
     flake-utils.lib.eachDefaultSystem (system:
   
+   let coq = coq-master.defaultPackage.${system}; in
    rec {
 
-    packages.default = self.packages.${system}.vscoq-language-server;
+    packages.default = self.packages.${system}.vscoq-language-server-coq-8-19;
 
-    packages.vscoq-language-server =
+    packages.vscoq-language-server-coq-8-18 =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { inherit system; };
+      let ocamlPackages = ocaml-ng.ocamlPackages_4_14; in
+      ocamlPackages.buildDunePackage {
+        duneVersion = "3";
+        pname = "vscoq-language-server";
+        version = "2.0.3";
+        src = ./language-server;
+        buildInputs = [
+          coq_8_18
+          dune_3
+        ] ++ (with coq.ocamlPackages; [
+          lablgtk3-sourceview3
+          glib
+          gnome.adwaita-icon-theme
+          wrapGAppsHook
+          ocaml
+          yojson
+          zarith
+          findlib
+          ppx_inline_test
+          ppx_assert
+          ppx_sexp_conv
+          ppx_deriving
+          ppx_optcomp
+          ppx_import
+          sexplib
+          ppx_yojson_conv
+          lsp
+          sel
+        ]);
+      };
+
+      packages.vscoq-language-server-coq-8-19 =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { inherit system; };
+      let ocamlPackages = ocaml-ng.ocamlPackages_4_14; in
+      ocamlPackages.buildDunePackage {
+        duneVersion = "3";
+        pname = "vscoq-language-server";
+        version = "2.0.3";
+        src = ./language-server;
+        buildInputs = [
+          coq_8_19
+          dune_3
+        ] ++ (with coq.ocamlPackages; [
+          lablgtk3-sourceview3
+          glib
+          gnome.adwaita-icon-theme
+          wrapGAppsHook
+          ocaml
+          yojson
+          zarith
+          findlib
+          ppx_inline_test
+          ppx_assert
+          ppx_sexp_conv
+          ppx_deriving
+          ppx_optcomp
+          ppx_import
+          sexplib
+          ppx_yojson_conv
+          lsp
+          sel
+        ]);
+      };
+
+    packages.vscoq-language-server-coq-master =
       # Notice the reference to nixpkgs here.
       with import nixpkgs { inherit system; };
       let ocamlPackages = ocaml-ng.ocamlPackages_4_14; in
@@ -68,18 +140,32 @@
         buildInputs = self.packages.${system}.vscoq-client.buildInputs;
       };
 
-    devShells.vscoq-language-server = 
+    devShells.vscoq-language-server-coq-8-18 = 
+        with import nixpkgs { inherit system; };
+        mkShell {
+            buildInputs =
+            self.packages.${system}.vscoq-language-server-coq-8-18.buildInputs;
+        };
+        
+    devShells.vscoq-language-server-8-19 = 
       with import nixpkgs { inherit system; };
       mkShell {
         buildInputs =
-          self.packages.${system}.vscoq-language-server.buildInputs;
+          self.packages.${system}.vscoq-language-server-coq-8-19.buildInputs;
+      };
+    
+    devShells.vscoq-language-server-coq-master = 
+      with import nixpkgs { inherit system; };
+      mkShell {
+        buildInputs =
+          self.packages.${system}.vscoq-language-server-coq-master.buildInputs;
       };
 
     devShells.default = 
       with import nixpkgs { inherit system; };
       mkShell {
         buildInputs =
-          self.packages.${system}.vscoq-language-server.buildInputs
+          self.packages.${system}.vscoq-language-server-coq-8-19.buildInputs
           ++ (with ocamlPackages; [
             ocaml-lsp
           ]);
