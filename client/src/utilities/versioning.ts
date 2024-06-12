0495dc0ff638c6ebf77bc79f28e9a9381c9fd104
Romain Tetley <romain.tetley@inria.fr>
2024-06-12 15:46:51 +0200

    Bump version for release

--- client/src/utilities/versioning.ts
+++ client/src/utilities/versioning.ts
@@ -36,6 +36,7 @@ const versionRequirements : VersionReq = {
     '2.1.0': '2.0.3',
     '2.1.1': '2.1.1',
     '2.1.2': '2.1.2',
+    '2.1.3': '2.1.3',
 };
 
 //We will add version ranges as we start releasing
