a1cc15f6ecc99ee77866a8f19f8fd781c80a17c8
Romain Tetley <romain.tetley@inria.fr>
2024-12-13 15:25:38 +0100

    feat: update version for pre-release

--- client/src/utilities/versioning.ts
+++ client/src/utilities/versioning.ts
@@ -42,6 +42,7 @@ const versionRequirements : VersionReq = {
     '2.1.7': '2.1.7',
     '2.2.0': '2.1.7',
     '2.2.1': '2.2.1',
+    '2.2.2': '2.2.2',
 };
 
 //We will add version ranges as we start releasing
